7b9b7a013a32e2ca5d41b6afba51abf272d9fb97
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-05-01 13:26:11 +0530

    Update decancer.py

--- decancer/decancer.py
+++ decancer/decancer.py
@@ -651,7 +651,7 @@ class Decancer(commands.Cog):
             guild = ctx.guild
             await self.decancer_log(guild, user, ctx.author, m_nick, new_cool_nick, "decancer")
             try:
-                await ctx.message.add_reaction("\N{WHITE HEAVY CHECK MARK}") 
+                await ctx.message.add_reaction("\N{<​:bc_x_goldverify:769899133174939648>}") 
             except discord.NotFound:
                 pass
 
